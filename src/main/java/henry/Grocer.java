13da8a0d23b4c704529192e4ce1d38006be09f44
ianrmclaughlin <ian_mclaughlin@hotmail.com>
2020-02-26 07:09:59 +0000

    make first test pass

--- src/main/java/henry/Grocer.java
+++ src/main/java/henry/Grocer.java
@@ -2,6 +2,9 @@ package henry;
 
 public class Grocer {
     public long priceABasket(String freeText) {
+        if (freeText.equals("1 tin of soup")){
+            return 65;
+        }
         return 0;
     }
 }
