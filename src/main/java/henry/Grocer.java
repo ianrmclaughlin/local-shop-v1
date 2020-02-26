8f8608811bc27b71fcd20dbcb81525b1020883f9
ianrmclaughlin <ian_mclaughlin@hotmail.com>
2020-02-26 08:31:27 +0000

    (last commit message should have read: "mixed multiple items") this commit - refactoring

--- src/main/java/henry/Grocer.java
+++ src/main/java/henry/Grocer.java
@@ -5,53 +5,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Grocer {
+
+    final int SOUP_PRICE = 65;
+    final int BREAD_PRICE = 80;
+    final int MILK_PRICE = 130;
+    final int APPLE_PRICE = 10;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public long priceABasket(String freeText) {
-        String tokenizedBasket = freeText
-                .replaceAll(" and "," ")
-                .replaceAll(","," ")
-                .replaceAll(" a "," 1 ")
-                .replaceAll(" an "," 1 ")
-                .replaceAll("^a ","1 ")
-                .replaceAll("^an ","1 ")
-                .replaceAll("tin.*soup",":soup,")
-                .replaceAll("loa.*bread",":bread,")
-                .replaceAll("bot.*milk",":milk,")
-                .replaceAll("apples?",":apple,")
-                .replaceAll(" ","")
-                ;
+        String tokenizedBasket = getTokenizedBasket(freeText);
+        Map<String, Long> orderMap = getOrderMap(tokenizedBasket);
+        long basketCost = getBasketCost(orderMap);
+        return basketCost;
+    }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private long getBasketCost(Map<String, Long> orderMap) {
+        long basketCost = 0;
+        Collection<String> cs = orderMap.keySet();
+        for (String itemName : cs) {
+            long itemCount = orderMap.get(itemName);
+            if (itemName.equals("soup")) {
+                basketCost = basketCost + itemCount * SOUP_PRICE;
+            }
+            if (itemName.equals("bread")) {
+                basketCost = basketCost + itemCount * BREAD_PRICE;
+            }
+            if (itemName.equals("milk")) {
+                basketCost = basketCost + itemCount * MILK_PRICE;
+            }
+            if (itemName.equals("apple")) {
+                basketCost = basketCost + itemCount * APPLE_PRICE;
+            }
+        }
+        return basketCost;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private Map<String, Long> getOrderMap(String tokenizedBasket) {
         String[] orderArray = tokenizedBasket.split(",");
-
-        Map<String,Long> orderMap = new HashMap<>();
-
-        for ( String orderLine : orderArray ) {
+        Map<String, Long> orderMap = new HashMap<>();
+        for (String orderLine : orderArray) {
             String[] a = orderLine.split(":");
             String itemName = a[1];
             Long itemCount = Long.parseLong(a[0]);
-            orderMap.put(itemName,itemCount);
+            orderMap.put(itemName, itemCount);
         }
+        return orderMap;
+    }
 
-        long basketCost = 0;
-
-        Collection<String> cs = orderMap.keySet();
-        for ( String itemName : cs ) {
-            long itemCount = orderMap.get(itemName);
-            if(itemName.equals("soup")){
-                basketCost = basketCost + itemCount * 65;
-            }
-            if(itemName.equals("bread")){
-                basketCost = basketCost + itemCount * 80;
-            }
-            if(itemName.equals("milk")){
-                basketCost = basketCost + itemCount * 130;
-            }
-            if(itemName.equals("apple")){
-                basketCost = basketCost + itemCount * 10;
-            }
-        }
-
-        return basketCost;
-
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private String getTokenizedBasket(String freeText) {
+        String tokenizedBasket = freeText
+                .replaceAll(" and ", " ")
+                .replaceAll(",", " ")
+                .replaceAll(" a ", " 1 ")
+                .replaceAll(" an ", " 1 ")
+                .replaceAll("^a ", "1 ")
+                .replaceAll("^an ", "1 ")
+                .replaceAll("tin.*soup", ":soup,")
+                .replaceAll("loa.*bread", ":bread,")
+                .replaceAll("bot.*milk", ":milk,")
+                .replaceAll("apples?", ":apple,")
+                .replaceAll(" ", "");
+        return tokenizedBasket;
     }
 }
