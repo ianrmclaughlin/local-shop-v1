c896ba6edad347ef0a9bbffe6c485486e8c17a84
ianrmclaughlin <ian_mclaughlin@hotmail.com>
2020-02-26 20:06:30 +0000

    added text output message

--- src/main/java/henry/Main.java
+++ src/main/java/henry/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Grocer g = new Grocer();
         String price = g.priceABasketWrapper(args);
+        System.out.println("Expected total cost = " + price);
     }
 }
