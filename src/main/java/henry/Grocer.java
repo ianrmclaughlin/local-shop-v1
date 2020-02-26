ed34eb56695f60d62777f8b00ece2430ea3fd68d
ianrmclaughlin <ian_mclaughlin@hotmail.com>
2020-02-26 07:13:13 +0000

    tests pass for multiple items

--- src/main/java/henry/Grocer.java
+++ src/main/java/henry/Grocer.java
@@ -5,6 +5,12 @@ public class Grocer {
         if (freeText.equals("1 tin of soup")){
             return 65;
         }
+        if (freeText.equals("2 tins of soup")){
+            return 130;
+        }
+        if (freeText.equals("3 tins of soup")){
+            return 195;
+        }
         return 0;
     }
 }
--- src/test/java/henry/GrocerTest.java
+++ src/test/java/henry/GrocerTest.java
@@ -10,4 +10,18 @@ public class GrocerTest {
         long basketPrice = grocer.priceABasket("1 tin of soup");
         Assert.assertEquals(65,basketPrice);
     }
+    @Test
+    public void givenTwoSoup_whenCalculateBasketPrice_thenReturnCorrectPrice(){
+        Grocer grocer = new Grocer();
+        long basketPrice = grocer.priceABasket("2 tins of soup");
+        Assert.assertEquals(130,basketPrice);
+    }
+    @Test
+    public void givenThreeSoup_whenCalculateBasketPrice_thenReturnCorrectPrice(){
+        Grocer grocer = new Grocer();
+        long basketPrice = grocer.priceABasket("3 tins of soup");
+        Assert.assertEquals(195,basketPrice);
+    }
+
+
 }
